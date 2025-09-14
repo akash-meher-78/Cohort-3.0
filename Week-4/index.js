6c4c28a64aab43b674c8a664b0956895a5c14837
akash-meher-78 <akashmeher6635@gmail.com>
2025-09-14 17:23:38 +0530

    Add Week4 and 4 offline

--- Week-4/index.js
+++ Week-4/index.js
@@ -1,5 +1,80 @@
-import chalk from 'chalk';
+import express from 'express'
+const app = express();
 
-console.log(chalk.blue('Hello, world!'));
-console.log(chalk.red.bold('This is an error message.'));
-console.log(chalk.green.underline('This is a success message.'));
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i<johnKidneys.length; i++) {
+        if (johnKidneys[i].healthy) {
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
+
+app.post("/", function(req, res) {
+    
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
+
+// 411
+app.put("/put", function(req, res) {
+    for (let i = 0; i<users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({});
+})
+
+// removing all the unhealhty kidneys
+app.delete("/delete", function(req, res) {
+    if(isThereAtleastOneUnhealthyKidney()) {
+        const newKidneys = [];
+        for (let i = 0; i<users[0].kidneys.length; i++) {
+            if (users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({msg: "done"})   
+    } else {
+        res.status(411).json({
+            msg: "You have no bad kidneys"
+        });
+    }
+})
+
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastOneUnhealthyKidney = false;
+    for (let i = 0; i<users[0].kidneys.length; i++) {
+        if (!users[0].kidneys[i].healthy) {
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney
+}
+app.listen(3000, () => {
+    console.log(`Example app listening on port 3000`)
+  })
